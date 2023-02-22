e23b03ddd647d08f0e1907554a643cafe3f9d597
ZenthWolf <ZenthRelin@gmail.com>
2023-02-22 03:50:32 -0500

    Setting up IInput and testing

--- src/pages/CampaignPage.vue
+++ src/pages/CampaignPage.vue
@@ -1,42 +1,25 @@
-<!--
- <template>
-    <q-page padding>
-        COMMENT CONTENT
-      <i-input class="q-mb-md" label="Campaign Name" v-model="campaign.data.name" />
+<template>
+  <q-page padding>
+  <!---->
+    <i-input class="q-mb-md" label="Campaign Name" v-model="campaign.data.name" />
+    Current Campaign Name: {{ campaign.data.name }}
+  </q-page>
+</template>
 
-      <q-expansion-item default-opened>
-        <template v-slot:header>
-          <div class="col sf-header text-h4 rounded-borders">
-            Factions<q-btn icon="add_circle" flat dense @click="addFaction" />
-          </div>
-        </template>
-        <faction
-          v-for="(f, i) in campaign.data.factions"
-          :key="i"
-          v-model="campaign.data.factions[i]"
-          @delete="removeFaction(i)"
-        />
-      </q-expansion-item>
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-      <q-expansion-item default-opened>
-        <template v-slot:header>
-          <div class="col sf-header text-h4 rounded-borders">Truths</div>
-        </template>
-        <truth label="Cataclysm" id="cataclysm" />
-        <truth label="Exodus" id="exodus" />
-        <truth label="Communities" id="communities" />
-        <truth label="Iron" id="iron" />
-        <truth label="Laws" id="laws" />
-        <truth label="Religion" id="religion" />
-        <truth label="Magic" id="magic" />
-        <truth label="Communication and Data" id="communication" />
-        <truth label="Medicine" id="medicine" />
-        <truth label="Artificial Intelligence" id="ai" />
-        <truth label="War" id="war" />
-        <truth label="Lifeforms" id="lifeforms" />
-        <truth label="Precursors" id="precursors" />
-        <truth label="Horrors" id="horrors" />
-      </q-expansion-item>
-    </q-page>
-  </template>
-  -->
+import IInput from 'src/components/IInput.vue'
+import { useCampaign } from 'src/stores/campaign'
+
+export default defineComponent({
+  name: 'CampaignPage',
+  components: { IInput },
+  setup () {
+    const campaign = useCampaign()
+    return {
+      campaign
+    }
+  }
+})
+</script>
