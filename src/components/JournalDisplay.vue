3dd90781083a3b54e40c50e9ab37473dd10fe1af
ZenthWolf <ZenthRelin@gmail.com>
2023-03-11 13:14:13 -0500

    Journal Entry Management

--- src/components/JournalDisplay.vue
+++ src/components/JournalDisplay.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="column items-center">
-    {{ `For ${campaign.data.name}` }}<br>
-    {{ `Journal title is ${campaign.data.journal[0].title}` }}<br>
-    {{ `Journal length is ${campaign.data.journal.length}` }}<br>
-  </div>
+  <hr color="#555555"/>
   <q-expansion-item header-class="q-py-none q-px-none" :default-opened="open" expand-icon-toggle>
     <template v-slot:header>
       <div class="row full-width items-center">
-        <i-input class="col-grow q-mr-sm" label="Title" v-model="campaign.data.journal[0].title" />
+        <span class="text-bold">{{ (index + 1).toString().padStart(3,'0') }}:</span>
+        <i-input class="col-grow q-ml-sm q-mr-sm" label="Title" v-model="campaign.data.journal[index].title" />
         <q-btn class="col-shrink" flat dense icon="info" @click="testFunction" />
         <!--<q-btn class="col-shrink" v-if="config.data.edit" flat dense icon="delete" @click="$emit('remove')">-->
         <q-btn class="col-shrink" flat dense icon="delete" @click="$emit('remove')">
@@ -15,10 +12,10 @@
         </q-btn>
       </div>
     </template>
-    <div class="q-mt-xs">
+    <div class="q-mt-xs q-mb-lg">
       <q-editor placeholder="Journal Body"
       content-class="journal-body"
-      v-model="campaign.data.journal[0].content"
+      v-model="campaign.data.journal[index].content"
       min-height="6rem"
       />
     </div>
@@ -33,14 +30,25 @@ import { useConfig } from 'src/stores/config'
 import IInput from 'src/components/IInput.vue'
 
 export default defineComponent({
-  name: 'JDisplay2',
+  name: 'JDisplay',
   components: { IInput },
+
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  emits: ['remove'],
 
   setup () {
     const campaign = useCampaign()
     const config = useConfig()
-
-    const open = false
 
     const testFunction = () => {
       console.log('buttonclicked')
@@ -48,8 +56,6 @@ export default defineComponent({
     return {
       campaign,
       config,
-
-      open,
 
       testFunction
     }
--- src/pages/Journaller.vue
+++ src/pages/Journaller.vue
@@ -1,20 +1,44 @@
 <template>
+  <q-btn label="new entry" @click="addJournal" />
+  <div class="column items-center">
+    <h3 style="margin: 0">Journal</h3><br>
+    {{ `First journal title is ${campaign.data.journal[0].title}` }}<br>
+    {{ `Journal length is ${campaign.data.journal.length}` }}<br>
+  </div>
   <!--<q-page class="column full-width items-center justify-evenly">-->
   <q-page padding>
-    <JDisplay />
+    <div v-for="(journal, index) in campaign.data.journal" :key="index">
+      <JDisplay
+        :index="index"
+        @remove="removeJournal(index)"
+      />
+  </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import { useCampaign } from 'src/stores/campaign'
+import { NewJournal } from 'src/lib/campaign'
 import JDisplay from 'src/components/JournalDisplay.vue'
 
 export default defineComponent({
   name: 'JournalIndex',
   components: { JDisplay },
   setup () {
+    const campaign = useCampaign()
+
+    const addJournal = () => { campaign.data.journal.push(NewJournal()) }
+    const removeJournal = (index: number) => {
+      if (window.confirm('Are you sure you want to delete this entry?')) campaign.data.journal.splice(index, 1)
+    }
+
     return {
+      campaign,
+
+      addJournal,
+      removeJournal
     }
   }
 })
