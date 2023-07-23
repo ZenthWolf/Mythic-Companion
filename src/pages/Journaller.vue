9eb01d2a805e450f046f93e787940ef89c28f554
ZenthWolf <ZenthRelin@gmail.com>
2023-07-23 17:23:06 -0400

    Chaos Factor increases changes with new journal

--- src/pages/Journaller.vue
+++ src/pages/Journaller.vue
@@ -1,6 +1,6 @@
 <template>
   <q-btn class="button" label="New +CF" @click="entryWithMoreChaos" />
-  <q-btn class="button" label="New" @click="addJournal" />
+  <q-btn class="button" label="New" @click="showNewDialogue = true" />
   <q-btn class="button" label="New -CF" @click="entryWithLessChaos" />
   <div class="column items-center">
     <h3 style="margin: 0">Journal</h3><br>
@@ -15,30 +15,94 @@
       />
   </div>
   </q-page>
+  <q-dialog v-model="showNewDialogue" :maximized="$q.platform.is.mobile">
+      <q-card>
+        <q-card-section class="text-center text-bold bg-secondary"> New Scene </q-card-section>
+        <q-card-section class="text-h6 text-center">Choose change to Chaos Factor</q-card-section>
+        <q-card-section>
+          <q-radio dense v-model="updateMethod" :val="ChaosUpdate.Decrease" label="Decrease" />
+          <q-radio dense v-model="updateMethod" :val="ChaosUpdate.Retain" label="Retain" />
+          <q-radio dense v-model="updateMethod" :val="ChaosUpdate.Increase" label="Increase" />
+          <q-radio dense v-model="updateMethod" :val="ChaosUpdate.Set" label="Set" />
+          <q-btn-dropdown
+            label="CF"
+            class="rounded q-ml-sm"
+          /><br>
+          {{ `You've selected: ${ChaosUpdate[updateMethod]}` }}
+        </q-card-section>
+
+        <q-card-actions align="center">
+          <q-btn
+            color="warning"
+            label="CLOSE"
+            @click="testFn()"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+
+import { defineComponent, ref } from 'vue'
 
 import { useCampaign } from 'src/stores/campaign'
 import { NewJournal } from 'src/lib/campaign'
 import JDisplay from 'src/components/JournalDisplay.vue'
+
+enum ChaosUpdate {
+      Decrease,
+      Retain,
+      Increase,
+      Set,
+      UNDEFINED
+    }
 
 export default defineComponent({
   name: 'JournalIndex',
   components: { JDisplay },
   setup () {
     const campaign = useCampaign()
+    const showNewDialogue = ref(false)
+
+    const updateMethod = ref(ChaosUpdate.UNDEFINED)
+    const selectedChaos = ref(undefined as (number | undefined))
+
+    const testFn = () => {
+      console.log(typeof updateMethod.value)
+      switch (updateMethod.value) {
+        case ChaosUpdate.Decrease:
+          console.log('Decr')
+          break
+        case ChaosUpdate.Retain:
+          console.log('Rtn')
+          break
+        case ChaosUpdate.Increase:
+          console.log('Incr')
+          break
+        case ChaosUpdate.Set:
+          console.log('Set')
+          break
+        case ChaosUpdate.UNDEFINED:
+          console.log('HELP HELP')
+          break
+        default:
+          console.log('HOLY FUCK THIS BROKE')
+          break
+      }
+      updateMethod.value = ChaosUpdate.UNDEFINED
+      showNewDialogue.value = false
+    }
 
     const entryWithMoreChaos = () => {
-      ++campaign.data.chaos_factor
+      campaign.increaseChaos()
       addJournal()
     }
 
     const addJournal = () => { campaign.data.journal.push(NewJournal(campaign.data.chaos_factor)) }
 
     const entryWithLessChaos = () => {
-      --campaign.data.chaos_factor
+      campaign.decreaseChaos()
       addJournal()
     }
 
@@ -50,6 +114,11 @@ export default defineComponent({
 
     return {
       campaign,
+      showNewDialogue,
+      ChaosUpdate,
+      selectedChaos,
+      updateMethod,
+      testFn,
 
       entryWithMoreChaos,
       addJournal,
